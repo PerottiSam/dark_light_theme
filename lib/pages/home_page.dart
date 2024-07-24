25bb1e545bcee38ecf6ac12f3095b4b8d7ad40b5
PerottiSam <s.perotti4@campus.unimib.it>
2024-07-24 20:52:20 +0200

    Finished product

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,29 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
+import 'package:dark_light_theme/components/box.dart';
+import 'package:dark_light_theme/components/button.dart';
+import 'package:dark_light_theme/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: MyBox(
+          child: MyButton(
+            color: Theme.of(context).colorScheme.secondary,
+            onTap: () {
+              Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+            },
+          ),
+          color: Theme.of(context).colorScheme.primary
+        ),
+      ),
+    );
   }
 }
